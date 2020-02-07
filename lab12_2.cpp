6abdcf65fa59d771238d2405a9e0cb6cda8af5f9
shoukugeki02 <59277996+shoukugeki02@users.noreply.github.com>
2020-02-07 17:59:08 +0700

    Update lab12_2.cpp

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -23,3 +23,34 @@ int main()
 }
 
 // Write definition of updateImage() and showImage() here
+
+void updateImage(bool image[N][M],int s,int x,int y){
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < M; j++){
+            if (sqrt(pow(i-x,2)+pow(j-y,2))<= s-1){
+                image[i][j] = 1;
+            }
+        }
+    }
+} 
+void showImage(const bool image[N][M]){
+    for (int x = 0 ; x < M+2; x++){
+    	cout << "-";
+	}
+	cout << "\n";
+	for (int i = 0; i < N; i++){
+		cout << "|";
+        for (int j = 0; j < M; j++){
+            if(image[i][j]==1){
+                cout << "*";
+            }else{
+                cout << " ";
+            }
+        } 
+        cout << "|"<< "\n";
+    }
+    for (int x = 0 ; x < M+2; x++){
+    	cout << "-";
+    }
+    cout << "\n";
+}
